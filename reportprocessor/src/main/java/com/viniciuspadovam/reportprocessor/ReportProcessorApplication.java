ca79d59742c4a47a92447ec488746558d5967ec3
viniciuspadovam <padovam3373@gmail.com>
2025-04-16 23:01:47 -0300

    feat(processor): initial report processor application

---
+++ reportprocessor/src/main/java/com/viniciuspadovam/reportprocessor/ReportProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.viniciuspadovam.reportprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportProcessorApplication.class, args);
+	}
+
+}
